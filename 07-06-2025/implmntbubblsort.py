69fe528b4c2cb57a58f3b59082717cab3a23e649
rgdevengineer <ritwickghosh.cse@gmail.com>
2025-06-07 20:22:36 +0530

    Add Daily Folders

---
+++ 07-06-2025/implmntbubblsort.py
@@ -0,0 +1,12 @@
+
+# write a python function to implement bubble sort
+
+def bubble_sort(data_list):
+    for r in range(1, len(data_list)):
+        for i in range(len(data_list)-r):
+            if data_list[i] > data_list[i+1]:
+                data_list[i],data_list[i+1] = data_list[i+1],data_list[i]
+
+l = [34,67,12,89,25,50]
+bubble_sort(l)
+print(l)
