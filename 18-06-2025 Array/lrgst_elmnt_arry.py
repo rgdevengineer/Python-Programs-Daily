dc4819b1cb7e77c9e041bf6f20c5ba4683eb7d9d
rgdevengineer <ritwickghosh.cse@gmail.com>
2025-06-18 18:57:36 +0530

    New Folder and Files added

---
+++ 18-06-2025 Array/lrgst_elmnt_arry.py
@@ -0,0 +1,47 @@
+
+'''
+Given an array of integers nums, return the value of the largest element in the array
+Examples:
+Input: nums = [3, 3, 6, 1]
+Output: 6
+Explanation: The largest element in array is 6
+Input: nums = [3, 3, 0, 99, -40]
+Output: 99
+Explanation: The largest element in array is 99
+'''
+
+'''
+class Solution:
+    def largestElement(self, arr):
+        largest = arr[0]
+        for num in arr:
+            if num > largest:
+                largest = num
+        return largest
+    
+Sol = Solution()
+print(Sol.largestElement([3, 3, 6, 1]))
+print(Sol.largestElement([3, 3, 0, 99, -40]))
+'''
+
+# The previous one was better but if I want to add testing then
+
+# Adding Basic Manual Testing Now
+
+class Solution:
+    def largestElement(self, arr):
+        if not arr:
+            raise ValueError("Array is Empty")
+        largest = arr[0]
+        for num in arr:
+            if num > largest:
+                largest = num
+        return largest
+        
+# Manual Testing
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.largestElement([3, 3, 6, 1]))
+    print(sol.largestElement([3, 3, 0, 99, -40]))
+    print(sol.largestElement([-5, -1, -10]))
